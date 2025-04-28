ea0fe88aedf1b0b1040ea6fbc5168fda7ab55107
root <root@edge-vm2.maas>
2025-04-28 22:56:10 +0000

    Added separate script for building docker images

--- prepare-nodes.sh
+++ prepare-nodes.sh
@@ -6,4 +6,3 @@ sysctl -w net.ipv6.conf.all.seg6_enabled=1
 sysctl -w net.ipv6.conf.default.seg6_enabled=1
 sysctl -w net.ipv6.conf.all.seg6_require_hmac=0
 
-docker build --network=host -t brunodzogovic/frr:latest .
