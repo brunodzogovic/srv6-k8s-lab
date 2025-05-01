e494153850efc30267ca798d1ba9782db7617be7
root <root@edge-vm2.maas>
2025-05-01 07:39:12 +0000

    fixed cilium issues for cluster1

--- cluster1/setup-cluster1.sh
+++ cluster1/setup-cluster1.sh
@@ -61,21 +61,24 @@ source "$(dirname "$0")/../cluster.env"
 
 helm repo add cilium https://helm.cilium.io/ || true
 helm repo update
-helm install cilium cilium/cilium --version "$CILIUM_VERSION" \
+helm install cilium cilium/cilium \
   --namespace kube-system --create-namespace \
+  --version "$CILIUM_VERSION" \
   --set installCRDs=true \
   --set ipam.mode=cluster-pool \
-  --set cluster.name=${CLUSTER_NAME} \
-  --set cluster.id=${CLUSTER_ID} \
-  --set bgpControlPlane.enabled=true \
-  --set ipv6.enabled=true \
-  --set ipv4.enabled=true \
   --set routingMode=native \
-  --set ipv6NativeRoutingCIDR=${POD_SUBNET_V6} \
-  --set ipv4NativeRoutingCIDR=${POD_SUBNET_V4} \
-  --set enableIPv4Masquerade=true \
-  --set enableIPMasqAgent=false \
-  --set autoDirectNodeRoutes=true
+  --set autoDirectNodeRoutes=true \
+  --set ipv6.enabled=true \
+  --set ipv6NativeRoutingCIDR=$POD_SUBNET_V6 \
+  --set clusterPoolIPv6PodCIDR=$POD_SUBNET_V6 \
+  --set clusterPoolIPv6MaskSize=64 \
+  --set ipv4.enabled=true \
+  --set ipv4NativeRoutingCIDR=$POD_SUBNET_V4 \
+  --set clusterPoolIPv4PodCIDR=$POD_SUBNET_V4 \
+  --set clusterPoolIPv4MaskSize=16 \
+  --set cluster.name=$CLUSTER_NAME \
+  --set cluster.id=$CLUSTER_ID \
+  --set bgpControlPlane.enabled=true
 EOF
 chmod +x "$CILIUM_INSTALL_SCRIPT"
 
